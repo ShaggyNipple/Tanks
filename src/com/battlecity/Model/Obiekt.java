a45001bd7038ba666c0485e9da6f13923b50a860
KB1101 <kornelgcc@gmail.com>
2016-04-12 21:11:43 +0200

    nadal nadal nadal cos tam robie

--- src/com/battlecity/Model/Obiekt.java
+++ src/com/battlecity/Model/Obiekt.java
@@ -4,6 +4,11 @@ package com.battlecity.Model;
  * Created by Kornel on 2016-04-12.
  */
 public abstract class Obiekt {
+    protected static final char ktoCzolg = 'C';
+    protected static final char ktoCegla = 'M';
+    protected static final char ktoKamien ='B';
+    protected static final char ktoZArosla = 'K';
+
     protected int wytrzymalosc;
     protected int X;
     protected int Y;
@@ -21,5 +26,28 @@ public abstract class Obiekt {
         this.kto = kto_char;
         this.wytrzymalosc = wytrzymalosc_int;
     }
+
+    public void setWytrzymalosc(int wyt){
+        this.wytrzymalosc = wyt;
+    }
+    public int getWytrzymalosc(){
+        return this.wytrzymalosc;
+    }
+
+    public void setX(int x){
+        this.X = x;
+    }
+    public void setY(int y){
+        this.Y = y;
+    }
+
+    public int getX(){
+        return this.X;
+    }
+
+    public int getY(){
+        return this.Y;
+    }
+
     //public abstract void akcja();
 }
