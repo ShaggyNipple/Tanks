2d5c6fb2c7fa11c446771a3c9c1651bbf4b29598
pioteer66 <pioteer.66@gmail.com>
2016-04-12 16:18:01 +0200

    Dodano testowy do Blok tak tak

--- src/com/battlecity/Main.java
+++ src/com/battlecity/Main.java
@@ -3,11 +3,9 @@ package com.battlecity;
 public class Main {
 
     public static void main(String[] args) {
-	// KORNEL LALALALA
 
         System.gc();
     while(true){
-        // test
     }
 
     }
