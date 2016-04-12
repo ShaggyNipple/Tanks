a555cf86fab2beab4c26b4209734a0ab349481fa
pioteer66 <pioteer.66@gmail.com>
2016-04-12 16:51:44 +0200

    Dodano zmienną do kamien

--- src/com/battlecity/Model/Kamien.java
+++ src/com/battlecity/Model/Kamien.java
@@ -3,4 +3,15 @@ package com.battlecity.Model;
 public class Kamien extends Blok {
     private int kamienistosc;
 
+    public Kamien(int kamienistosc) {
+        this.kamienistosc = kamienistosc;
+    }
+
+    public int getKamienistosc() {
+        return kamienistosc;
+    }
+
+    public void setKamienistosc(int kamienistosc) {
+        this.kamienistosc = kamienistosc;
+    }
 }
