4ada3f057d38beeb056a579a574762bcb22ea665
pioteer66 <pioteer.66@gmail.com>
2016-04-12 17:02:19 +0200

    test

--- src/com/battlecity/Main.java
+++ src/com/battlecity/Main.java
@@ -3,9 +3,13 @@ package com.battlecity;
 public class Main {
 
     public static void main(String[] args) {
+	// write your code here
 
-        System.gc();
+
+       System.gc();
     while(true){
+        // test
+        //kolejny test
     }
 
     }
