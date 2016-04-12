5a034168b068f38278d19ae7bdec34151c70280d
KB1101 <kornelgcc@gmail.com>
2016-04-12 15:35:29 +0200

    Merge remote-tracking branch 'remotes/origin/Dawid' into Kornel
    # Conflicts:
    #	.idea/vcs.xml

--- src/com/battlecity/Main.java
+++ src/com/battlecity/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.gc();
 
-
-        // dupa
+        while(true){
+         // p
+        }
     }
 }
